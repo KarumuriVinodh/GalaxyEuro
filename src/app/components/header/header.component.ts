d34d895a6d5b613ab952830975c355a647d269cb
Vinodh Karumuri <karumuri.vinodh@gmail.com>
2020-01-08 01:42:14 +0530

    Added Header
    Design Header & Conatct Me parts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+
+
+
   constructor() { }
 
-  ngOnInit() {
+ 
+
+  @HostListener('window:scroll', ['$event'])
+
+  onWindowScroll(e) {
+    let element = document.querySelector('.navbar');
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('sticky-top' );
+    } else {
+      element.classList.remove('sticky-top' );
+    }
   }
 
+
+  ngOnInit() { }
 }
