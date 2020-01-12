13ece872003b5a512ee7dd272e594b66bedaee8e
Vinodh Karumuri <karumuri.vinodh@gmail.com>
2020-01-12 10:41:10 +0530

    Added Home Page
    Added Home Page

--- src/app/components/about-company/about-company.component.ts
+++ src/app/components/about-company/about-company.component.ts
@@ -12,4 +12,9 @@ export class AboutCompanyComponent implements OnInit {
   ngOnInit() {
   }
 
+  // Open Company Address in google maps in new browser window
+  openAddress(){
+    window.open('https://www.google.com/maps/place/GalaxyEuro/@17.5667265,78.4817987,17z/data=!3m1!4b1!4m8!1m2!2m1!1sGalaxy+Euro!3m4!1s0x0:0x794d6a2c38704079!8m2!3d17.5667213!4d78.4839872', "_blank");
+  }
+
 }
