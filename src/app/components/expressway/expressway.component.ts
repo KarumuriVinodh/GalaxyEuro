5ad4f0da4318c107bb77a51d9e5bfa3eddc17423
Teja Prasad Behara <53400473+Tejaprasadhub@users.noreply.github.com>
2020-02-11 08:57:50 +0530

    Create expressway.component.ts

---
+++ src/app/components/expressway/expressway.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expressway',
+  templateUrl: './expressway.component.html',
+  styleUrls: ['./expressway.component.scss']
+})
+export class ExpresswayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
