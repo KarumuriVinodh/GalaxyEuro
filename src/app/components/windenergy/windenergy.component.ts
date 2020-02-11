e05c24fc0e253878d47795c1b34ff8bf7e530804
Teja Prasad Behara <53400473+Tejaprasadhub@users.noreply.github.com>
2020-02-11 09:29:57 +0530

    Create windenergy.component.ts

---
+++ src/app/components/windenergy/windenergy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-windenergy',
+  templateUrl: './windenergy.component.html',
+  styleUrls: ['./windenergy.component.scss']
+})
+export class WindenergyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
